b1ad108e09dabbee7feae2cca404db38725c9592
elangovanshanthi <elangovan2me@gmail.com>
2018-10-28 19:59:28 +0530

    added

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,16 @@ const users = [
 const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://10secondsofcode.github.io', // Your website URL
+  baseUrl: '/10seconds/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
-
+  //    https://10secondsofcode.github.io/10seconds/ 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  
+  projectName: '10seconds',
+  organizationName: '10secondsofcode',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
